b113fe912c204fc7f2d9e845ec8586bb1024c08e
Sylvester Esso Nmakwe <59395101+Sylvester10@users.noreply.github.com>
2025-10-03 13:26:49 +0100

    Restructure

--- application/third_party/stripe/secrets.php
+++ application/third_party/stripe/secrets.php
@@ -3,4 +3,4 @@
 // or in a private script that does not publicly expose the source code.
 
 // This is your test secret API key.
-$stripeSecretKey = 'pk_live_51PRzxkE9sO0PVQExnVkHUosSNU59GpQJxePQHyzGFTw6WcIhtSjbnbFnCBAnMx4P2buRSRsJ8syPOO58zaMLLksq00cZUk6aqy';
+$stripeSecretKey = '';
