8c5e3faf3565576b7c67ebfe41dd9cc53414fe02
sylvesteresso <onyekaesso10@gmail.com>
2025-10-13 11:15:09 +0100

    updated helper

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -1798,6 +1798,26 @@ if (! function_exists('addSeperator')) {
 	}
 }
 
+function stripe_key_test() {
+	$key_test = 'sk_test_51PRzxkE9sO0PVQExtCKFGTwx9UbyA1fzuEUkRJb72WMqnoSo5LrmoLLPo5kadv9G2ngotyzPfb4zk4hpNuJyWeQZ00nfLbPTng';
+	return $key_test;
+}
+
+function stripe_key_live() {
+	$key_live = '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';
+	return $key_live;
+}
+
+function paystack_key_test() {
+	$key_test = 'sk_test_30121d635245ff14a9377984b6e7cfd2aa0efb55';
+	return $key_test;
+}
+
+function paystack_key_live() {
+	$key_live = 'sk_live_3d206640616308c6b859b0c9a75d557ecfa45827';
+	return $key_live;
+}
+
 function production_url($path = '') {
 	return trim('https://sharemybag.co.uk/'.$path);
 }
