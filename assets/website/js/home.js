3fa5a217c1b5ff0fe1a83c69697259802a50a311
sylvesteresso <onyekaesso10@gmail.com>
2025-12-09 11:00:24 +0100

    updated Admin

--- assets/website/js/home.js
+++ assets/website/js/home.js
@@ -1,44 +1,44 @@
 jQuery(document).ready(function ($) {
-  ("use strict");
+  ('use strict');
 
   /*=========== Disable Button ===========*/
   function disableSubmitBtn() {
-    var submitButton = $("#submit");
-    submitButton.addClass("disabled");
-    submitButton.attr("disabled", true); // Disables the button
+    var submitButton = $('#submit');
+    submitButton.addClass('disabled');
+    submitButton.attr('disabled', true); // Disables the button
   }
 
   /*=========== Enable Button ===========*/
   function enableSubmitBtn() {
-    var submitButton = $("#submit");
-    submitButton.removeClass("disabled");
-    submitButton.attr("disabled", false); // Enables the button
+    var submitButton = $('#submit');
+    submitButton.removeClass('disabled');
+    submitButton.attr('disabled', false); // Enables the button
   }
 
   // Close search results when clicking the close button (using event delegation)
-  $(document).on("click", ".search-back-drop", function () {
-    $("body").removeClass("search-active");
-    $("#search-results").html(""); // Clear search results
+  $(document).on('click', '.search-back-drop', function () {
+    $('body').removeClass('search-active');
+    $('#search-results').html(''); // Clear search results
   });
 
   //Search
-  $("#search_form").submit(function (e) {
+  $('#search_form').submit(function (e) {
     e.preventDefault();
-    $("#search-spinner").removeClass("d-none");
-    $("#search-results").html("");
-    let val = $("#select_destination").val();
-    let url = $(this).attr("action");
+    $('#search-spinner').removeClass('d-none');
+    $('#search-results').html('');
+    let val = $('#select_destination').val();
+    let url = $(this).attr('action');
 
-    if (val.trim() == "") {
+    if (val.trim() == '') {
       return;
     }
 
     let form_data = $(this).serialize();
     $.ajax({
       url: url,
-      type: "POST",
+      type: 'POST',
       data: form_data,
-      contentType: "application/x-www-form-urlencoded",
+      contentType: 'application/x-www-form-urlencoded',
       success: function (response) {
         response = JSON.parse(response);
         console.log(response);
@@ -90,9 +90,9 @@ jQuery(document).ready(function ($) {
 					</section>`;
 
           setTimeout(function () {
-            $("body").addClass("search-active");
-            $("#search-results").html(html_response);
-            $("#search-spinner").addClass("d-none");
+            $('body').addClass('search-active');
+            $('#search-results').html(html_response);
+            $('#search-spinner').addClass('d-none');
           }, 3000);
         } else {
           // Handle case when response.status is false (e.g., no results found)
@@ -105,12 +105,19 @@ jQuery(document).ready(function ($) {
 												<img src="${base_url}assets/website/icons/no-bag.png">
 												<h5>No Traveller Available</h5>
 											</div>
+
+                      <h6>
+                        <a href="${base_url}registration" class="main-btn primary wow fadeInUp animated" data-wow-delay=".15s" type="submit">
+                          Sign up to join the wait list
+                        </a>
+                      </h6>
 										</div>
+                    
 									</section>`;
           setTimeout(function () {
-            $("body").addClass("search-active");
-            $("#search-results").html(noResultsHtml);
-            $("#search-spinner").addClass("d-none");
+            $('body').addClass('search-active');
+            $('#search-results').html(noResultsHtml);
+            $('#search-spinner').addClass('d-none');
           }, 2000);
         }
       },
@@ -121,45 +128,45 @@ jQuery(document).ready(function ($) {
   });
 
   //Sign up
-  $("#signup_form").submit(function (e) {
+  $('#signup_form').submit(function (e) {
     e.preventDefault();
-    $("#search-spinner").removeClass("d-none");
+    $('#search-spinner').removeClass('d-none');
     var form_data = $(this).serialize();
-    var redirect_url = base_url + "verify-email";
+    var redirect_url = base_url + 'verify-email';
     disableSubmitBtn();
     $.ajax({
-      url: base_url + "registration/signup",
-      type: "POST",
+      url: base_url + 'registration/signup',
+      type: 'POST',
       data: form_data,
-      dataType: "json",
+      dataType: 'json',
       success: function (res) {
         // Delay success message for 5 seconds
         setTimeout(function () {
           if (res.status) {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-success text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast");
-            $("#signup_form")[0].reset(); //reset form fields
-            $("#search-spinner").addClass("d-none");
+              .fadeIn('fast');
+            $('#signup_form')[0].reset(); //reset form fields
+            $('#search-spinner').addClass('d-none');
             setTimeout(function () {
-              $(location).attr("href", redirect_url);
+              $(location).attr('href', redirect_url);
             }, 4000);
             enableSubmitBtn();
           } else {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-danger text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(5000)
-              .fadeOut("slow");
-            $("#search-spinner").addClass("d-none");
+              .fadeOut('slow');
+            $('#search-spinner').addClass('d-none');
             enableSubmitBtn();
           }
         }, 3000); // 5000 milliseconds (5 seconds) delay
@@ -168,45 +175,45 @@ jQuery(document).ready(function ($) {
   });
 
   //Verify email
-  $("#verify_email_form").submit(function (e) {
+  $('#verify_email_form').submit(function (e) {
     e.preventDefault();
-    $("#search-spinner").removeClass("d-none");
+    $('#search-spinner').removeClass('d-none');
     var form_data = $(this).serialize();
-    var redirect_url = base_url + "signin";
+    var redirect_url = base_url + 'signin';
     disableSubmitBtn();
     $.ajax({
-      url: base_url + "registration/verify_email_ajax",
-      type: "POST",
+      url: base_url + 'registration/verify_email_ajax',
+      type: 'POST',
       data: form_data,
-      dataType: "json",
+      dataType: 'json',
       success: function (res) {
         // Delay success message for 5 seconds
         setTimeout(function () {
           if (res.status) {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-success text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast");
-            $("#verify_email_form")[0].reset(); //reset form fields
-            $("#search-spinner").addClass("d-none");
+              .fadeIn('fast');
+            $('#verify_email_form')[0].reset(); //reset form fields
+            $('#search-spinner').addClass('d-none');
             setTimeout(function () {
-              $(location).attr("href", redirect_url);
+              $(location).attr('href', redirect_url);
             }, 4000);
             enableSubmitBtn();
           } else {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-danger text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(5000)
-              .fadeOut("slow");
-            $("#search-spinner").addClass("d-none");
+              .fadeOut('slow');
+            $('#search-spinner').addClass('d-none');
             enableSubmitBtn();
           }
         }, 2000); // 5000 milliseconds (5 seconds) delay
@@ -215,95 +222,95 @@ jQuery(document).ready(function ($) {
   });
 
   // Resend Verification email
-  $("#resend_verification_email").click(function () {
-    $("#search-spinners").removeClass("d-none"); // Show spinner
+  $('#resend_verification_email').click(function () {
+    $('#search-spinners').removeClass('d-none'); // Show spinner
 
     $.ajax({
-      url: base_url + "registration/resend_verification_email_ajax",
-      type: "POST",
-      dataType: "json",
+      url: base_url + 'registration/resend_verification_email_ajax',
+      type: 'POST',
+      dataType: 'json',
       success: function (res) {
         // Delay handling for the success message
         setTimeout(function () {
           if (res.status) {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-success text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(3000) // Show the success message for 5 seconds
-              .fadeOut("slow"); // Then fade it out
+              .fadeOut('slow'); // Then fade it out
           } else {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-danger text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(3000)
-              .fadeOut("slow");
+              .fadeOut('slow');
           }
-          $("#search-spinners").addClass("d-none"); // Hide spinner
+          $('#search-spinners').addClass('d-none'); // Hide spinner
         }, 2000); // 2000 milliseconds (2 seconds) delay
       },
       error: function () {
-        $("#status_msg")
+        $('#status_msg')
           .html(
             '<div class="alert alert-danger text-center" style="color: #000">' +
-              "An error occurred. Please try again." +
-              "</div>"
+              'An error occurred. Please try again.' +
+              '</div>'
           )
-          .fadeIn("fast")
+          .fadeIn('fast')
           .delay(5000)
-          .fadeOut("slow");
-        $("#search-spinners").addClass("d-none"); // Hide spinner
+          .fadeOut('slow');
+        $('#search-spinners').addClass('d-none'); // Hide spinner
       },
     });
   });
 
   //Recover Password
-  $("#recover_password_form").submit(function (e) {
+  $('#recover_password_form').submit(function (e) {
     e.preventDefault();
-    $("#search-spinner").removeClass("d-none");
+    $('#search-spinner').removeClass('d-none');
     var form_data = $(this).serialize();
-    var redirect_url = base_url + "signin";
+    var redirect_url = base_url + 'signin';
     disableSubmitBtn();
     $.ajax({
-      url: base_url + "recover_password/password_recovery_ajax",
-      type: "POST",
+      url: base_url + 'recover_password/password_recovery_ajax',
+      type: 'POST',
       data: form_data,
-      dataType: "json",
+      dataType: 'json',
       success: function (res) {
         // Delay success message for 5 seconds
         setTimeout(function () {
           if (res.status) {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-success text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast");
-            $("#recover_password_form")[0].reset(); //reset form fields
-            $("#search-spinner").addClass("d-none");
+              .fadeIn('fast');
+            $('#recover_password_form')[0].reset(); //reset form fields
+            $('#search-spinner').addClass('d-none');
             setTimeout(function () {
-              $(location).attr("href", redirect_url);
+              $(location).attr('href', redirect_url);
             }, 5000);
             enableSubmitBtn();
           } else {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-danger text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(5000)
-              .fadeOut("slow");
-            $("#search-spinner").addClass("d-none");
+              .fadeOut('slow');
+            $('#search-spinner').addClass('d-none');
             enableSubmitBtn();
           }
         }, 2000); // 5000 milliseconds (5 seconds) delay
@@ -312,45 +319,45 @@ jQuery(document).ready(function ($) {
   });
 
   //Change Password
-  $("#change_pass_form").submit(function (e) {
+  $('#change_pass_form').submit(function (e) {
     e.preventDefault();
-    $("#search-spinner").removeClass("d-none");
+    $('#search-spinner').removeClass('d-none');
     var form_data = $(this).serialize();
-    var redirect_url = base_url + "signin";
+    var redirect_url = base_url + 'signin';
     disableSubmitBtn();
     $.ajax({
-      url: base_url + "recover_password/change_password_ajax",
-      type: "POST",
+      url: base_url + 'recover_password/change_password_ajax',
+      type: 'POST',
       data: form_data,
-      dataType: "json",
+      dataType: 'json',
       success: function (res) {
         // Delay success message for 5 seconds
         setTimeout(function () {
           if (res.status) {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-success text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast");
-            $("#change_pass_form")[0].reset(); //reset form fields
-            $("#search-spinner").addClass("d-none");
+              .fadeIn('fast');
+            $('#change_pass_form')[0].reset(); //reset form fields
+            $('#search-spinner').addClass('d-none');
             setTimeout(function () {
-              $(location).attr("href", redirect_url);
+              $(location).attr('href', redirect_url);
             }, 5000);
             enableSubmitBtn();
           } else {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-danger text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(5000)
-              .fadeOut("slow");
-            $("#search-spinner").addClass("d-none");
+              .fadeOut('slow');
+            $('#search-spinner').addClass('d-none');
             enableSubmitBtn();
           }
         }, 2000); // 5000 milliseconds (5 seconds) delay
@@ -359,38 +366,38 @@ jQuery(document).ready(function ($) {
   });
 
   //User login
-  $("#user_login_form").submit(function (e) {
+  $('#user_login_form').submit(function (e) {
     e.preventDefault();
-    $("#search-spinner").removeClass("d-none");
+    $('#search-spinner').removeClass('d-none');
     var form_data = $(this).serialize();
-    var redirect_url = $("#requested_page").val();
+    var redirect_url = $('#requested_page').val();
     disableSubmitBtn();
     $.ajax({
-      url: base_url + "user_login/login_ajax",
-      type: "POST",
+      url: base_url + 'user_login/login_ajax',
+      type: 'POST',
       data: form_data,
-      dataType: "json",
+      dataType: 'json',
       success: function (res) {
         // Delay success message for 5 seconds
         setTimeout(function () {
           if (res.status) {
-            $("#user_login_form")[0].reset(); //reset form fields
-            $("#search-spinner").addClass("d-none");
+            $('#user_login_form')[0].reset(); //reset form fields
+            $('#search-spinner').addClass('d-none');
             setTimeout(function () {
-              $(location).attr("href", redirect_url);
+              $(location).attr('href', redirect_url);
             }, 900);
             enableSubmitBtn();
           } else {
-            $("#status_msg")
+            $('#status_msg')
               .html(
                 '<div class="alert alert-danger text-center" style="color: #000">' +
                   res.msg +
-                  "</div>"
+                  '</div>'
               )
-              .fadeIn("fast")
+              .fadeIn('fast')
               .delay(5000)
-              .fadeOut("slow");
-            $("#search-spinner").addClass("d-none");
+              .fadeOut('slow');
+            $('#search-spinner').addClass('d-none');
             enableSubmitBtn();
           }
         }, 3000); // 5000 milliseconds (5 seconds) delay
@@ -554,5 +561,4 @@ jQuery(document).ready(function ($) {
   //     }
   //   });
   // });
-
 });
