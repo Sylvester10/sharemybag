24dbb9ae8b074e747c2ce721c8e13a399213f6ba
sylvesteresso <onyekaesso10@gmail.com>
2026-02-03 13:06:57 +0100

    updated Admin

--- application/views/users/market_place.php
+++ application/views/users/market_place.php
@@ -3,7 +3,7 @@
     <div class="card">
         <div class="card-body">
             <div class="text-center">
-                <img src="<?php echo base_url(); ?>assets/users/images/illustrations/4133820.jpg" alt="flexy-img" class="img-fluid" width="500">
+                <img src="<?php echo base_url(); ?>assets/users/images/illustrations/4133820.jpg" alt="flexy-img" class="img-fluid" width="300">
 
                 <!-- <img src="<?php echo base_url(); ?>assets/users/images/backgrounds/school.png" alt="flexy-img" class="img-fluid" width="500"> -->
 
